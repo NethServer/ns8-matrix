fb6dccdbe7b452c8043c350112937eeda8badd1d
mrmarkuz <mrmarkuz666@gmail.com>
2025-11-20 12:01:39 +0100

    Fix validation and add max-per-node

--- build-images.sh
+++ build-images.sh
@@ -39,6 +39,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm cluster:accountconsumer" \
     --label="org.nethserver.tcp-ports-demand=4" \
     --label="org.nethserver.rootfull=0" \
+	--label="org.nethserver.max-per-node=1" \
     --label="org.nethserver.images=ghcr.io/dexidp/dex:v2.44.0-alpine docker.io/postgres:14.19-alpine ghcr.io/element-hq/synapse:v1.138.2 ghcr.io/element-hq/element-web:v1.12.0 ghcr.io/cinnyapp/cinny:v4.10.2" \
     "${container}"
 # Commit the image
